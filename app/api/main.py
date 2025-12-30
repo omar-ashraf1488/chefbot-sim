6a9099e519c288afd13966483594a4b66cc66442
Omar Ashraf Omar <o.ashraf1488@gmail.com>
2025-12-30 20:15:04 +0100

    first commit

---
+++ app/api/main.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from app.core.config import settings
+
+api_router = APIRouter()
+
+
+if settings.ENVIRONMENT == "local":
+    #api_router.include_router(private.router)
+    pass
